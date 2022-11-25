7df09ccee9e55368ba725992a4bc8213527d7474
Jalexandrou <jalexandrou10@gmail.com>
2022-11-25 12:27:32 +0000

    Updated ALU.sv to work with ControlUnit

--- Lab-4/ALU.sv
+++ Lab-4/ALU.sv
@@ -3,15 +3,15 @@ module ALU #(
 )(
     input  logic   [DATA_WIDTH-1:0] ALUop1,
     input  logic   [DATA_WIDTH-1:0] ALUop2,
-    input  logic                    ALUctrl,
+    input  logic              [2:0] ALUctrl,
     output logic   [DATA_WIDTH-1:0] sum,
     output logic                    eq
-)
+);
 
-    assign begin
-        if(ALUctrl == 1)
+    always_comb begin
+        if(ALUctrl == 0)
             sum <= ALUop1 + ALUop2;
-            if (ALUop1 = ALUop2)
+        if (ALUop1 = ALUop2)
                 eq <= 1;
     end
 
