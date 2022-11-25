d639545ad8f71e2a52e060c6992d830fa7b49a04
Jalexandrou <jalexandrou10@gmail.com>
2022-11-25 12:31:45 +0000

    Syntax correction

--- Lab-4/RedBlock.sv
+++ Lab-4/RedBlock.sv
@@ -1,18 +1,18 @@
 module RedBlock #(
     parameter ADDRESS_WIDTH = 32;
-    parameter DATA_WIDTH = 32:
+    parameter DATA_WIDTH = 32;
 )(
     input logic     [ADDRESS_WIDTH-1:0] rs1,
     input logic     [ADDRESS_WIDTH-1:0] rs2,
     input logic     [ADDRESS_WIDTH-1:0] rd,
     input logic                         RegWrite,
     input logic                         clk,
-    input logic     [DATA_WIDTH-1:0]    ImmOp,
-    input logic                         ALUsrc,
+    input logic        [DATA_WIDTH-1:0] ImmOp,
+    input logic                   [2:0] ALUsrc,
     input logic                         ALUctrl,
     output logic                        a0,
     output logic                        eq                    
-)
+);
     logic [DATA_WIDTH-1:0] ALUop1;
     logic [DATA_WIDTH-1:0] ALUop2;
     logic [DATA_WIDTH-1:0] regOp2;
@@ -28,13 +28,13 @@ module RedBlock #(
         .rd1 (ALUop1),
         .rd2 (regOp2),
         .a0 (a0)
-    )
+    );
 
     mux mux2 (
         .regOp2 (regOp2),
         .ImmOp (ImmOp),
         .ALUop2 (ALUop2)
-    )
+    );
 
     ALU adder (
         .ALUop1 (ALUop1),
@@ -42,6 +42,6 @@ module RedBlock #(
         .sum (ALUout),
         .eq (eq),
         .ALUctrl (ALUctrl)
-    )
+    );
 
 endmodule
--- Lab-4/mux.sv
+++ Lab-4/mux.sv
@@ -7,7 +7,7 @@ module mux #(
     input logic  [DATA_WIDTH-1:0] ImmOp,
     input logic                   ALUsrc,
     output logic [DATA_WIDTH-1:0] ALUop2
-)
+);
 
 assign ALUop2 = ALUsrc ? ImmOp : regOp2;
 
