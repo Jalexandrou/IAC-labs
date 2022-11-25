1a00f4f644ba28bd08b9501610c25c3a79988412
derinak21 <116320304+derinak21@users.noreply.github.com>
2022-11-25 10:37:38 +0000

    Create inc_PC.sv
    Added inc_PC unit

---
+++ Lab-4/inc_PC.sv
@@ -0,0 +1,17 @@
+module next_PC #(
+    parameter WIDTH = 32
+)(
+    // interface signals
+    input logic             PC,         //PC cycle
+    input logic             ImmOp,      //Immediate Operand 
+    input logic             PCsrc,      //PC src 
+    output logic [WIDTH-1:0] next_PC    //next PC output
+);
+
+    logic branch_PC, inc_PC;
+
+    assign branch_PC= PC + ImmOp;
+    assign inc_PC= PC + 3'b100;
+    assign next_PC= PCsrc ? branch_PC : inc_PC;
+    
+endmodule
