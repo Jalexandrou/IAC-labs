4e191fe273f4e611d00c593c42ff07da1137e9f5
Jacob <59978422+JacobWhiteHat@users.noreply.github.com>
2022-11-25 13:50:41 +0000

    Added Extra Functions to ALU
    Added subtract, and, or and SLT functions to ALU.

--- Lab-4/ALU.sv
+++ Lab-4/ALU.sv
@@ -3,16 +3,25 @@ module ALU #(
 )(
     input  logic   [DATA_WIDTH-1:0] ALUop1,
     input  logic   [DATA_WIDTH-1:0] ALUop2,
-    input  logic              [2:0] ALUctrl,
-    output logic   [DATA_WIDTH-1:0] sum,
+    input  logic   [2:0]            ALUctrl,
+    output logic   [DATA_WIDTH-1:0] ALUOut,
     output logic                    eq
 );
 
+    typedef enum bit[2:0] {add, subtract, and, or, slt = 5} func;
+
     always_comb begin
-        if(ALUctrl == 0)
-            sum <= ALUop1 + ALUop2;
-        if (ALUop1 = ALUop2)
-                eq <= 1;
-    end
+        case(ALUctrl)
+            add: ALUOut = ALUop1 + ALUop2;
+            subtract: ALUOut = ALUop1 - ALUop2;
+            and: ALUOut = ALUop1 & ALUop2;
+            or: ALUOut = ALUop1 | ALUop2;
+            slt: ALUOut = (ALUop1 < ALUop2) ? 1 : 0;
+            default: ALUOut = ALUop1 + ALUop2;
+        endcase
+        if (ALUop1 = ALUop2) eq = 1;
+        else                 eq = 0;
+    end;
+
 
 endmodule
